ae2fd5adb88f2eb67456545b4afb390df14b30d1
Luke Klinker <lklinker1@gmail.com>
2014-11-08 17:40:08 -0600

    [Fix] Force close

--- src/main/java/com/klinker/android/twitter/settings/configure_pages/ConfigurationPagerAdapter.java
+++ src/main/java/com/klinker/android/twitter/settings/configure_pages/ConfigurationPagerAdapter.java
@@ -53,7 +53,7 @@ public class ConfigurationPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return TimelinePagerAdapter.MAX_EXTRA_PAGES + 1;
+        return TimelinePagerAdapter.MAX_EXTRA_PAGES;
     }
 
     @Override
