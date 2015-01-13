201fc4af2bca20ea4d7ddb21e8cf41fd3dcf2a2e
Luke Klinker <lklinker1@gmail.com>
2015-01-12 19:05:53 -0600

    [Fix] change the way it saves a trends search

--- src/main/java/com/klinker/android/twitter/ui/drawer_activities/discover/trends/SearchedTrendsActivity.java
+++ src/main/java/com/klinker/android/twitter/ui/drawer_activities/discover/trends/SearchedTrendsActivity.java
@@ -249,13 +249,8 @@ public class SearchedTrendsActivity extends Activity {
     private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             searchQuery = intent.getStringExtra(SearchManager.QUERY);
-            searchQuery += " -RT";
 
             String query = searchQuery;
-
-            if (query.contains("\"")) {
-                doSearch(query);
-            }
 
             SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
                     MySuggestionsProvider.AUTHORITY, MySuggestionsProvider.MODE);
@@ -276,6 +271,14 @@ public class SearchedTrendsActivity extends Activity {
                     source.createTag(searchQuery.replaceAll("\"", ""));
                 }
             }
+
+            searchQuery += " -RT";
+
+            if (query.contains("\"")) {
+                doSearch(query);
+            }
+
+
         }
     }
 
